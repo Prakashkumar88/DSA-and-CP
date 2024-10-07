bf8acbebd85a2590326e370603731a05cfb9795e
Prakash Kumar <143504731+Prakashkumar88@users.noreply.github.com>
2024-10-07 18:58:56 +0530

    Permutation_with_Spaces.cpp
    Recursively generates permutations of the input string ip by either:
    -Adding an underscore (_) before the next character.
    -Adding the character directly without an underscore.
    -The function accumulates the generated strings in the vector v.

---
+++ Recusion/Permutation_with_Spaces.cpp
@@ -0,0 +1,130 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Add the necessary include paths to the compiler flags
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization("unroll-loops")
+
+#define nl '\n'
+#define sp ' '
+#define pi 2 * acos(0.0)
+const int INF = 1e9 + 10;
+
+// Control Flow
+#define FOR(i, a, b) for (int i = a; i < (b); i++)
+#define f0r(i, a) for (int i = 0; i < (a); i++)
+#define fOrd(i, a, b) for (int i = (b) - 1; i >= a; i--)
+#define f0rd(i, a) for (int i = (a) - 1; i >= 0; i--)
+#define trav(a, x) for (auto &a : x)
+
+// Types
+#define ui unsigned int
+#define us unsigned short
+#define ull unsigned long long
+#define ll long long
+#define ld long double
+#define vstr vector<string>
+#define vll vector<ll>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+
+// Utils
+#define len(x) int((x).size())
+#define pb push_back
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+
+// Constants
+const ll MOD = 1e9 + 7;
+
+// Helper Functions
+ll nCk(ll n, ll k)
+{
+    ll res = 1;
+    for (ll i = 0; i < k; i++)
+    {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return res;
+}
+ll lcm(ll a, ll b)
+{
+    return a / __gcd(a, b) * b;
+}
+ll power(ll a, ll b, ll mod)
+{
+    if (b == 0)
+        return 1;
+    ll res = power(a, b / 2, mod);
+    res = (res * res) % mod;
+    if (b % 2 != 0)
+        res = (res * a) % mod;
+    return res;
+}
+/*
+    ___        __              __   ______          __        _____ __             __          __  __
+   /   | _____/ /___  ______ _/ /  / ____/___  ____/ /__     / ___// /_____ ______/ /______   / / / /__  ________
+  / /| |/ ___/ __/ / / / __ `/ /  / /   / __ \/ __  / _ \    \__ \/ __/ __ `/ ___/ __/ ___/  / /_/ / _ \/ ___/ _ \
+ / ___ / /__/ /_/ /_/ / /_/ / /  / /___/ /_/ / /_/ /  __/   ___/ / /_/ /_/ / /  / /_(__  )  / __  /  __/ /  /  __/
+/_/  |_\___/\__/\__,_/\__,_/_/   \____/\____/\__,_/\___/   /____/\__/\__,_/_/   \__/____/  /_/ /_/\___/_/   \___/
+*/
+void permutation(string ip, string op, vstr &v)
+{
+    if (ip.length() == 0)
+    {
+        v.pb(op);
+        return;
+    }
+    string op1 = op;
+    string op2 = op;
+
+    op1.pb('_');
+    op1.pb(ip[0]);
+    op2.pb(ip[0]);
+
+    ip.erase(ip.begin() + 0);
+
+    permutation(ip, op1, v);
+    permutation(ip, op2, v);
+}
+
+void solve()
+{
+    string s;
+    cin >> s;
+
+    vector<string> v;
+
+    string ip = s;
+    string op = "";
+
+    op.pb(ip[0]);
+    ip.erase(ip.begin());
+
+    permutation(ip, op, v);
+
+    sort(v.begin(), v.end());
+    for (auto &res : v)
+    {
+        cout << res << sp;
+    }
+    cout << nl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
