f73c4fc0ef54a19b00a99c424ba8c593171aecbf
Prakash Kumar <143504731+Prakashkumar88@users.noreply.github.com>
2024-10-26 17:30:15 +0530

    Update kSwapLargestN.cpp
    example attached

--- BackTracking/kSwapLargestN.cpp
+++ BackTracking/kSwapLargestN.cpp
@@ -113,6 +113,15 @@ void solve()
     kSwapLargestN(str, k, 0, ans);
 
     cout << ans << nl;
+    // Input:
+    // K = 4
+    // str = "1234567"
+    // Output:
+    // 7654321
+    // Explanation:
+    // Three swaps can make the
+    // input 1234567 to 7654321, swapping 1
+    // with 7, 2 with 6 and finally 3 with 5
 }
 
 int main()
