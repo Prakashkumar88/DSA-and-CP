3438f95929af6920d3e48c748367c2fa7b755e46
Prakash Kumar <143504731+Prakashkumar88@users.noreply.github.com>
2024-10-31 12:19:01 +0530

    Create breakPalindrom.cpp

---
+++ BackTracking/breakPalindrom.cpp
@@ -0,0 +1,122 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Add the necessary include paths to the compiler flags
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization("unroll-loops")
+
+#define nl '\n'
+#define sp ' '
+#define pi 2 * acos(0.0)
+const int INF = 1e9 + 10;
+
+// Control Flow
+#define FOR(i, a, b) for (int i = a; i < (b); i++)
+#define f0r(i, a) for (int i = 0; i < (a); i++)
+#define fOrd(i, a, b) for (int i = (b)-1; i >= a; i--)
+#define f0rd(i, a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a, x) for (auto &a : x)
+
+// Types
+#define ui unsigned int
+#define us unsigned short
+#define ull unsigned long long
+#define ll long long
+#define ld long double
+#define vstr vector<string>
+#define vll vector<ll>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+
+// Utils
+#define len(x) int((x).size())
+#define pb push_back
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+
+// Constants
+const ll MOD = 1e9 + 7;
+
+// Helper Functions
+ll nCk(ll n, ll k){
+    ll res = 1;
+    for (ll i = 0; i < k; i++)
+    {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return res;
+}
+ll lcm(ll a, ll b) {
+    return a / __gcd(a, b) * b;
+}
+ll power(ll a, ll b, ll mod) {
+    if (b == 0) return 1;
+    ll res = power(a, b / 2, mod);
+    res = (res * res) % mod;
+    if (b % 2 != 0) res = (res * a) % mod;
+    return res;
+}
+/*
+    ___        __              __   ______          __        _____ __             __          __  __
+   /   | _____/ /___  ______ _/ /  / ____/___  ____/ /__     / ___// /_____ ______/ /______   / / / /__  ________
+  / /| |/ ___/ __/ / / / __ `/ /  / /   / __ \/ __  / _ \    \__ \/ __/ __ `/ ___/ __/ ___/  / /_/ / _ \/ ___/ _ \
+ / ___ / /__/ /_/ /_/ / /_/ / /  / /___/ /_/ / /_/ /  __/   ___/ / /_/ /_/ / /  / /_(__  )  / __  /  __/ /  /  __/
+/_/  |_\___/\__/\__,_/\__,_/_/   \____/\____/\__,_/\___/   /____/\__/\__,_/_/   \__/____/  /_/ /_/\___/_/   \___/
+*/
+bool is_palindrom(const string &s) {
+    int n = s.size();
+    for (int i = 0; i < n / 2; i++) {
+        if (s[i] != s[n - 1 - i]) return false;
+    }
+    return true;
+}
+
+void breakPalindrom(string &s, vstr &temp, vector<vstr> &ans, int idx) {
+    if (idx == s.size()) {
+        ans.pb(temp);
+        return;
+    }
+
+    string pal;
+    for (int i = idx; i < len(s); i++) {
+        pal += s[i];
+        if (is_palindrom(pal)) {
+            temp.pb(pal);
+            breakPalindrom(s, temp, ans, i + 1);
+            temp.pop_back();
+        }
+    }
+}
+
+void solve() {
+    string s;
+    cin >> s;
+
+    vector<vstr> results;
+    vstr temp;                                                           //input - 
+    breakPalindrom(s, temp, results, 0);                           // aabccc
+                                                                        //output - 
+    for (const auto &v : results) {                                     // a a b c c c 
+        trav(e, v) {                                                    // a a b c cc 
+            cout << e << sp;                                            // a a b cc c 
+        }                                                               // a a b ccc 
+        cout << nl;                                                     // aa b c c c 
+    }                                                                   // aa b c cc 
+}                                                                       // aa b cc c 
+                                                                        // aa b ccc 
+int main() {                                                            // aa b cc c 
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
