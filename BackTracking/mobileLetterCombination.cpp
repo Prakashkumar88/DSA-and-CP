f99f01f4bfe67fd8ca38839fc2a21be957226266
Prakash Kumar <143504731+Prakashkumar88@users.noreply.github.com>
2024-11-09 11:36:18 +0530

    mobileLetterCombination.cpp
    Given a string containing digits from 2-9 inclusive, return all possible letter combinations that the number could represent. Return the answer in any order.
    
    A mapping of digits to letters (just like on the telephone buttons)

---
+++ BackTracking/mobileLetterCombination.cpp
@@ -0,0 +1,134 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Add the necessary include paths to the compiler flags
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization("unroll-loops")
+
+#define nl '\n'
+#define sp ' '
+#define pi 2 * acos(0.0)
+const int INF = 1e9 + 10;
+
+// Control Flow
+#define FOR(i, a, b) for (int i = a; i < (b); i++)
+#define f0r(i, a) for (int i = 0; i < (a); i++)
+#define fOrd(i, a, b) for (int i = (b) - 1; i >= a; i--)
+#define f0rd(i, a) for (int i = (a) - 1; i >= 0; i--)
+#define trav(a, x) for (auto &a : x)
+
+// Types
+#define ui unsigned int
+#define us unsigned short
+#define ull unsigned long long
+#define ll long long
+#define ld long double
+#define vstr vector<string>
+#define vll vector<ll>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+
+// Utils
+#define len(x) int((x).size())
+#define pb push_back
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+
+// Constants
+const ll MOD = 1e9 + 7;
+
+// Helper Functions
+ll nCk(ll n, ll k)
+{
+    ll res = 1;
+    for (ll i = 0; i < k; i++)
+    {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return res;
+}
+ll lcm(ll a, ll b)
+{
+    return a / __gcd(a, b) * b;
+}
+ll power(ll a, ll b, ll mod)
+{
+    if (b == 0)
+        return 1;
+    ll res = power(a, b / 2, mod);
+    res = (res * res) % mod;
+    if (b % 2 != 0)
+        res = (res * a) % mod;
+    return res;
+}
+/*
+    ___        __              __   ______          __        _____ __             __          __  __
+   /   | _____/ /___  ______ _/ /  / ____/___  ____/ /__     / ___// /_____ ______/ /______   / / / /__  ________
+  / /| |/ ___/ __/ / / / __ `/ /  / /   / __ \/ __  / _ \    \__ \/ __/ __ `/ ___/ __/ ___/  / /_/ / _ \/ ___/ _ \
+ / ___ / /__/ /_/ /_/ / /_/ / /  / /___/ /_/ / /_/ /  __/   ___/ / /_/ /_/ / /  / /_(__  )  / __  /  __/ /  /  __/
+/_/  |_\___/\__/\__,_/\__,_/_/   \____/\____/\__,_/\___/   /____/\__/\__,_/_/   \__/____/  /_/ /_/\___/_/   \___/
+*/
+
+void helper(int idx, string &digits, map<char, string> &mp, string &temp, vstr &ans)
+{
+    if (idx == len(digits))
+    {
+        ans.pb(temp);
+        return;
+    }
+
+    char ch = digits[idx];
+    for (char c : mp[ch])
+    {
+        temp += c;
+        helper(idx + 1, digits, mp, temp, ans);
+        temp.pop_back(); // backtrack to add more
+    }
+}
+void solve()
+{
+    string digits;
+    cin >> digits;
+
+    vstr ans;
+    if (len(digits) == 0)
+    {
+        trav(e, ans)
+        {
+            cout << e << sp;
+        }
+        cout << nl;
+        return;
+    }
+
+    map<char, string> mp;
+    mp['2'] = "abc", mp['3'] = "def", mp['4'] = "ghi", mp['5'] = "jkl";
+    mp['6'] = "mno", mp['7'] = "pqrs", mp['8'] = "tuv", mp['9'] = "wxyz";
+
+    string temp;
+    helper(0, digits, mp, temp, ans);
+
+    trav(e, ans)
+    {
+        cout << e << sp;                     //Input: digits = "23"
+    }                                        //Output: ["ad","ae","af","bd","be","bf","cd","ce","cf"]    
+    cout << nl;
+}
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    int t;
+    cin >> t;
+    while (t--)
+        solve();
+
+    return 0;
+}
