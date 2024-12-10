5309fbf71fd38cb3e75ca10cf66bd6402c5cc6fb
Prakash Kumar <143504731+Prakashkumar88@users.noreply.github.com>
2024-12-10 14:41:51 +0530

    Create BinaryTreeDiameter.cpp
    Given a binary tree, the task is to determine the diameter of the tree. The diameter/width of a tree is defined as the number of edges on the longest path between any two nodes

---
+++ DynamicProgramming/BinaryTreeDiameter.cpp
@@ -0,0 +1,116 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Add the necessary include paths to the compiler flags
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization("unroll-loops")
+
+#define nl '\n'
+#define sp ' '
+#define pi 2 * acos(0.0)
+const int INF = 1e9 + 10;
+
+// Control Flow
+#define FOR(i, a, b) for (int i = a; i < (b); i++)
+#define f0r(i, a) for (int i = 0; i < (a); i++)
+#define fOrd(i, a, b) for (int i = (b) - 1; i >= a; i--)
+#define f0rd(i, a) for (int i = (a) - 1; i >= 0; i--)
+#define trav(a, x) for (auto &a : x)
+
+// Types
+#define ui unsigned int
+#define us unsigned short
+#define ull unsigned long long
+#define ll long long
+#define ld long double
+#define vstr vector<string>
+#define vll vector<ll>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+
+// Utils
+#define len(x) int((x).size())
+#define pb push_back
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+
+// Constants
+const ll mod = 1e9 + 7;
+
+// Helper Functions
+ll nCk(ll n, ll k)
+{
+    ll res = 1;
+    for (ll i = 0; i < k; i++)
+    {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return res;
+}
+ll lcm(ll a, ll b)
+{
+    return a / __gcd(a, b) * b;
+}
+ll power(ll a, ll b, ll mod)
+{
+    if (b == 0)
+        return 1;
+    ll res = power(a, b / 2, mod);
+    res = (res * res) % mod;
+    if (b % 2 != 0)
+        res = (res * a) % mod;
+    return res;
+}
+/*
+    ___        __              __   ______          __        _____ __             __          __  __
+   /   | _____/ /___  ______ _/ /  / ____/___  ____/ /__     / ___// /_____ ______/ /______   / / / /__  ________
+  / /| |/ ___/ __/ / / / __ `/ /  / /   / __ \/ __  / _ \    \__ \/ __/ __ `/ ___/ __/ ___/  / /_/ / _ \/ ___/ _ \
+ / ___ / /__/ /_/ /_/ / /_/ / /  / /___/ /_/ / /_/ /  __/   ___/ / /_/ /_/ / /  / /_(__  )  / __  /  __/ /  /  __/
+/_/  |_\___/\__/\__,_/\__,_/_/   \____/\____/\__,_/\___/   /____/\__/\__,_/_/   \__/____/  /_/ /_/\___/_/   \___/
+*/
+struct TreeNode {
+    int val;
+    TreeNode* left;
+    TreeNode* right;
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+};
+
+int diameter(TreeNode *root, int res)
+{
+    if(root == nullptr) return 0;
+
+    int leftHeight = diameter(root->left, res);
+    int rightHeight = diameter(root->right, res);
+
+    int temp = max(leftHeight, rightHeight) + 1;
+    int ans = leftHeight + rightHeight + 1;
+    res = max(res, ans);
+
+    return temp;
+}
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    // int t;
+    // cin >> t;
+    // while (t--)
+    // {
+        TreeNode *root = new TreeNode(1);
+        root->left = new TreeNode(2);
+        root->right = new TreeNode(3);
+        root->left->left = new TreeNode(4);
+        root->left->right = new TreeNode(5);
+        int res = 0;
+        res = diameter(root, res);
+        cout << res << nl;
+    // }
+
+    return 0;
+}
