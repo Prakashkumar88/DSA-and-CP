3c0a8ca2fe794bfbe66ce95024916694e9d027ff
Prakash Kumar <143504731+Prakashkumar88@users.noreply.github.com>
2025-02-06 12:50:06 +0530

    Create rootToNode.cpp
    print the path from root to node using dfs

---
+++ Trees/rootToNode.cpp
@@ -0,0 +1,126 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+// Add the necessary include paths to the compiler flags
+#pragma GCC optimize("Ofast")
+#pragma GCC target("avx,avx2,fma")
+#pragma GCC optimization("unroll-loops")
+
+#define nl '\n'
+#define sp ' '
+#define pi 2 * acos(0.0)
+const int INF = 1e9 + 10;
+
+// Control Flow
+#define FOR(i, a, b) for (int i = a; i < (b); i++)
+#define f0r(i, a) for (int i = 0; i < (a); i++)
+#define fOrd(i, a, b) for (int i = (b)-1; i >= a; i--)
+#define f0rd(i, a) for (int i = (a)-1; i >= 0; i--)
+#define trav(a, x) for (auto &a : x)
+
+// Types
+#define ui unsigned int
+#define us unsigned short
+#define ull unsigned long long
+#define ll long long
+#define ld long double
+#define vstr vector<string>
+#define vll vector<ll>
+#define vi vector<int>
+#define vvi vector<vector<int>>
+#define vii vector<pair<int, int>>
+#define pii pair<int, int>
+
+// Utils
+#define len(x) int((x).size())
+#define pb push_back
+#define all(n) n.begin(), n.end()
+#define rall(n) n.rbegin(), n.rend()
+
+// Constants
+const ll mod = 1e9 + 7;
+
+// Helper Functions
+ll nCk(ll n, ll k){
+    ll res = 1;
+    for (ll i = 0; i < k; i++)
+    {
+        res = res * (n - i);
+        res = res / (i + 1);
+    }
+    return res;
+}
+ll lcm(ll a, ll b) {
+    return a / __gcd(a, b) * b;
+}
+ll power(ll a, ll b, ll mod) {
+    if (b == 0) return 1;
+    ll res = power(a, b / 2, mod);
+    res = (res * res) % mod;
+    if (b % 2 != 0) res = (res * a) % mod;
+    return res;
+}
+/*
+    ___        __              __   ______          __        _____ __             __          __  __
+   /   | _____/ /___  ______ _/ /  / ____/___  ____/ /__     / ___// /_____ ______/ /______   / / / /__  ________
+  / /| |/ ___/ __/ / / / __ `/ /  / /   / __ \/ __  / _ \    \__ \/ __/ __ `/ ___/ __/ ___/  / /_/ / _ \/ ___/ _ \
+ / ___ / /__/ /_/ /_/ / /_/ / /  / /___/ /_/ / /_/ /  __/   ___/ / /_/ /_/ / /  / /_(__  )  / __  /  __/ /  /  __/
+/_/  |_\___/\__/\__,_/\__,_/_/   \____/\____/\__,_/\___/   /____/\__/\__,_/_/   \__/____/  /_/ /_/\___/_/   \___/
+*/
+
+map<int, int> mp; // To store parent of each node
+void dfs(int node, vi adj[], int par) {
+    mp[node] = par;
+    trav(child, adj[node]) {
+        if (child != par) {
+            dfs(child, adj, node);
+        }
+    }
+}
+
+void print_path(int x) {
+    vi path;
+    while (x != 0) {          // Traverse from node x to root
+        path.push_back(x);
+        x = mp[x];            // Move to parent node
+    }
+    reverse(path.begin(), path.end()); // Reverse to get root-to-x path
+    trav(node, path) cout << node << ' ';
+    cout << nl;
+}
+
+void solve() {
+    int n;
+    cin >> n;
+
+    vi adj[n + 1];
+    for (int i = 0; i < n - 1; i++) {
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        adj[v].push_back(u);
+    }
+
+    dfs(1, adj, 0);
+
+    int x;
+    cin >> x;     
+    print_path(x);
+}
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    auto start = chrono::high_resolution_clock::now();
+    // int t;
+    // cin >> t;
+    // while(t--)
+        solve();
+
+    auto end = chrono::high_resolution_clock::now();
+    auto duration = chrono::duration_cast<chrono::milliseconds>(end - start);
+    cerr << "Execution Time: " << duration.count() << " ms";
+    return 0;
+}
